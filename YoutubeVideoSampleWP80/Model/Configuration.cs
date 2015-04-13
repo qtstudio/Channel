633fe81c177b747209c1f78a407f23d2ce88f163
Tạng Nguyễn <1012374@student.hcmus.edu.vn>
2015-04-13 14:27:14 +0700

    read config json

---
+++ YoutubeVideoSampleWP80/Model/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace YoutubeVideoSampleWP80.Model
+{
+    public class Configuration
+    {
+        public Configuration() { }
+
+        public Configuration(string jsonConfigFile)
+        {
+        }
+        [JsonProperty(PropertyName = "index")]
+        public int Index { get; set; }
+        [JsonProperty(PropertyName = "maxResult")]
+        public int MaxResult { get; set; }
+        [JsonProperty(PropertyName = "channelId")]
+        public string ChannelId { get; set; }
+        [JsonProperty(PropertyName = "orderBy")]
+        public string OrderBy { get; set; }
+        [JsonProperty(PropertyName = "typeData")]
+        public string TypeData { get; set; }
+        [JsonProperty(PropertyName = "token")]
+        public string Token { get; set; }
+        [JsonProperty(PropertyName = "query")]
+        public string Query { get; set; }
+
+        public static Configuration CreateConfig(string sourceJson)
+        {
+            return JsonConvert.DeserializeObject<Configuration>(sourceJson);
+        }
+    }
+}
