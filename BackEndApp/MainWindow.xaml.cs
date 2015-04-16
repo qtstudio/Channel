1e0584a22e97b1692a01b25d4c8c8e641cc50897
Tạng Nguyễn <1012374@student.hcmus.edu.vn>
2015-04-16 22:41:17 +0700

    Add db online, build auto

--- BackEndApp/MainWindow.xaml.cs
+++ BackEndApp/MainWindow.xaml.cs
@@ -24,5 +24,21 @@ namespace BackEndApp
         {
             InitializeComponent();
         }
+
+        private void BtnBuildAllApp(object sender, RoutedEventArgs e)
+        {
+            var si = new System.Diagnostics.Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    FileName = "cmd.exe",
+                    Arguments =
+                        "/K cd C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC & vcvarsall.bat & CD E:\\YoutubeVideoSampleWP80\\BaseApp.csproj  /p:Configuration=Release"
+                }
+            };
+            si.Start();
+            si.Close();
+        }
     }
 }
